d14f56f7640523c8d05ba75e036e8e1ae5a1183a
harrykuskus <harrykusumabhakti@gmail.com>
2022-06-13 00:27:19 +0800

    Add sintax

--- promise.js
+++ promise.js
@@ -1,7 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const ixx = await promiseTheaterIXX();
+  const vgc = await promiseTheaterVGC();
+  const data = await ixx.concat(vgc);
+  return data.filter((x) => x.hasil == emosi).length;
+};
 
 module.exports = {
   promiseOutput,
